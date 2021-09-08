cf97eeda7a86585347184efce990e77615a664a9
amisha26 <amisha8299@gmail.com>
2021-09-08 15:47:33 +0530

    table.js route added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./comp/Login";
 import Header from "./comp/Header";
+import Table from "./comp/Table";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/" component={Login} />
+          <Route path="/data" component={Table} />
         </Switch>
       </div>
     </Router>
